7c8e68409e0f27f12c3fbe64e80655b243571380
Irving <irvinggiles10@gmail.com>
2018-09-21 13:23:19 +1000

    FinishedChallange

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+
+   
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        private Entities db = new Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult YourModules()
+        {
+            ViewBag.Message = "Here are the modules you are responsible for:";
+
+            var userId = User.Identity.GetUserId();
+            List<UserModule> modules = new List<UserModule>(db.UserModules.Where(u => u.Id == userId)) ;
+
+            ViewBag.Modules = modules;
+
+            return View();
+        }
+          
+        
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/UserModulesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserModulesController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: UserModules
+        public ActionResult Index()
+        {
+            return View(db.UserModules.ToList());
+        }
+
+        // GET: UserModules/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserModule userModule = db.UserModules.Find(id);
+            if (userModule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userModule);
+        }
+
+        // GET: UserModules/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserModules/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,MacAddress,DateIssued")] UserModule userModule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserModules.Add(userModule);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userModule);
+        }
+
+        // GET: UserModules/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserModule userModule = db.UserModules.Find(id);
+            if (userModule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userModule);
+        }
+
+        // POST: UserModules/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,MacAddress,DateIssued")] UserModule userModule)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userModule).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userModule);
+        }
+
+        // GET: UserModules/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserModule userModule = db.UserModules.Find(id);
+            if (userModule == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userModule);
+        }
+
+        // POST: UserModules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            UserModule userModule = db.UserModules.Find(id);
+            db.UserModules.Remove(userModule);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
